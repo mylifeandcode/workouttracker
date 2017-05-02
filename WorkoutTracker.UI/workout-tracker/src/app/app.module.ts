b128718677e14703181649c1233e93bb83e82988
Alan C. Hummel <ahummel@MAINDB.ABIM.ORG>
2017-05-02 13:03:00 -0400

    Added routing, Bootstrap, and Font Awesome

--- WorkoutTracker.UI/workout-tracker/src/app/app.module.ts
+++ WorkoutTracker.UI/workout-tracker/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
