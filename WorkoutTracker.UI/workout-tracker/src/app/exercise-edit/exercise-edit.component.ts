b948a922135e9afe61719884516ac51e5fa9f94c
Alan C. Hummel <ahummel@MAINDB.ABIM.ORG>
2017-06-08 12:53:54 -0400

    Checkboxes!

--- WorkoutTracker.UI/workout-tracker/src/app/exercise-edit/exercise-edit.component.ts
+++ WorkoutTracker.UI/workout-tracker/src/app/exercise-edit/exercise-edit.component.ts
@@ -1,6 +1,6 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { ExerciseService } from '../exercise.service';
 import { Exercise } from '../exercise';
 import { TargetArea } from '../target-area';
@@ -16,7 +16,7 @@ export class ExerciseEditComponent implements OnInit {
         this.createForm();
     }
 
-    public targetAreas: Array<TargetArea>;
+    public targetAreas: Array<TargetArea> = [];
 
     exercise: Exercise;
     exerciseForm: FormGroup;
@@ -27,14 +27,43 @@ export class ExerciseEditComponent implements OnInit {
     }
 
     setupViewModel() {
+        //TODO: Merge available target areas with selected target areas
+        this.targetAreas.push(new TargetArea(1, "Chest", null, null, null, null, false));
+        this.targetAreas.push(new TargetArea(1, "Biceps", null, null, null, null, false));
+        this.targetAreas.push(new TargetArea(1, "Triceps", null, null, null, null, false));
+        this.targetAreas.push(new TargetArea(1, "Shoulders", null, null, null, null, false));
+        this.targetAreas.push(new TargetArea(1, "Core", null, null, null, null, false));
+        this.targetAreas.push(new TargetArea(1, "Legs", null, null, null, null, false));
     }
 
     createForm() {
         this.exerciseForm = this._formBuilder.group({
             id: [0, Validators.required ], //TODO: Get ID from URL
             name: [ '', Validators.required ], 
-            description: ['', Validators.required]
+            description: ['', Validators.required], 
+            targetAreas: this.buildTargetAreas()
         });
+
+        
+
+        let checkboxes = new Array<FormControl>(this.targetAreas.length);
+        //this.exercise.targetAreas.forEach(element => {
+          //  checkboxes.push(new FormControl()
+        //});
+
+        //let checkboxArray = new FormArray()
     }
 
+    buildTargetAreas() : FormArray {
+        //Great approach I found at:
+        //https://netbasal.com/handling-multiple-checkboxes-in-angular-forms-57eb8e846d21
+        const arr = this.targetAreas.map(area => {
+            return this._formBuilder.control(area.selected);
+        });
+        return this._formBuilder.array(arr);
+    }
+
+    get allTargetAreas() {
+        return this.exerciseForm.get('targetAreas');
+    };
 }
--- WorkoutTracker.UI/workout-tracker/src/app/target-area.ts
+++ WorkoutTracker.UI/workout-tracker/src/app/target-area.ts
@@ -1,2 +1,23 @@
-export class TargetArea {
+import { NamedEntity } from './named-entity';
+
+export class TargetArea extends NamedEntity {
+    
+    constructor (
+        id: number, 
+        name: string, 
+        createdBy: string, 
+        createdDateTime: Date, 
+        modifiedBy: string, 
+        modifiedDateTime: Date, 
+        public selected: boolean) {
+
+            super();
+            this.id = id;
+            this.name = name;
+            this.createdBy = createdBy;
+            this.createdDateTime = createdDateTime;
+            this.modifiedBy = modifiedBy;
+            this.modifiedDateTime = modifiedDateTime;
+
+    }
 }
