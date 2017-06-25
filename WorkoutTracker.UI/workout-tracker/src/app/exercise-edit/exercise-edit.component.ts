7a20ea1405ebdb54d37409e2913b23643b6cf270
mylifeandcode <mylifeandcode@gmail.com>
2017-06-25 12:09:35 -0400

    Fixed Target Areas checkboxes in Exercise Edit. Menu clean-up.

--- WorkoutTracker.UI/workout-tracker/src/app/exercise-edit/exercise-edit.component.ts
+++ WorkoutTracker.UI/workout-tracker/src/app/exercise-edit/exercise-edit.component.ts
@@ -13,7 +13,8 @@ import { TargetArea } from '../target-area';
 export class ExerciseEditComponent implements OnInit {
 
     constructor(private route: ActivatedRoute, private _formBuilder: FormBuilder, private _exerciseSvc: ExerciseService) {
-        this.createForm();
+        console.log("Constructing ExerciseEditComponent...");
+        //this.createForm();
     }
 
     public targetAreas: Array<TargetArea> = [];
@@ -24,24 +25,31 @@ export class ExerciseEditComponent implements OnInit {
     ngOnInit() {
         //this._exerciseSvc.getTargetAreas().subscribe((resp) => this.targetAreas = resp);
         this.setupViewModel();
+        this.createForm();
     }
 
     setupViewModel() {
         //TODO: Merge available target areas with selected target areas
-        this.targetAreas.push(new TargetArea(1, "Chest", null, null, null, null, false));
-        this.targetAreas.push(new TargetArea(1, "Biceps", null, null, null, null, false));
-        this.targetAreas.push(new TargetArea(1, "Triceps", null, null, null, null, false));
-        this.targetAreas.push(new TargetArea(1, "Shoulders", null, null, null, null, false));
-        this.targetAreas.push(new TargetArea(1, "Core", null, null, null, null, false));
-        this.targetAreas.push(new TargetArea(1, "Legs", null, null, null, null, false));
+        console.log("Setting up view model...");
+        this.targetAreas.push(new TargetArea(1, "Abs", null, null, null, null, false));
+        this.targetAreas.push(new TargetArea(2, "Back", null, null, null, null, false));
+        this.targetAreas.push(new TargetArea(3, "Biceps", null, null, null, null, false));
+        this.targetAreas.push(new TargetArea(4, "Chest", null, null, null, null, false));
+        this.targetAreas.push(new TargetArea(5, "Core", null, null, null, null, false));
+        this.targetAreas.push(new TargetArea(6, "Legs", null, null, null, null, false));
+        this.targetAreas.push(new TargetArea(7, "Shoulders", null, null, null, null, false));
+        this.targetAreas.push(new TargetArea(8, "Triceps", null, null, null, null, false));
     }
 
     createForm() {
+        console.log("Creating form...");
+
+        //Use FormBuilder to create our root FormGroup
         this.exerciseForm = this._formBuilder.group({
-            id: [0, Validators.required ], //TODO: Get ID from URL
+            id: [0, Validators.required ], //TODO: Get ID from URL. 0 for new, actual ID for existing exercise.
             name: [ '', Validators.required ], 
             description: ['', Validators.required], 
-            targetAreas: this.buildTargetAreas()
+            targetAreas: this.buildTargetAreas() //Get our checkbox FormControls
         });
 
         
@@ -58,6 +66,7 @@ export class ExerciseEditComponent implements OnInit {
         //Great approach I found at:
         //https://netbasal.com/handling-multiple-checkboxes-in-angular-forms-57eb8e846d21
         const arr = this.targetAreas.map(area => {
+            console.log("area:", area);
             return this._formBuilder.control(area.selected);
         });
         return this._formBuilder.array(arr);
