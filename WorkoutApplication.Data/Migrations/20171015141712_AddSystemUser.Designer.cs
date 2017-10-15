e809fb6a8b66758a45bc5e27bae81dc28215f257
mylifeandcode <mylifeandcode@gmail.com>
2017-10-15 12:00:17 -0400

    Added constraint on CreatedByUserId to TargetAreas tabke and associated migration

--- WorkoutApplication.Data/Migrations/20171015141712_AddSystemUser.Designer.cs
+++ WorkoutApplication.Data/Migrations/20171015141712_AddSystemUser.Designer.cs
@@ -12,9 +12,10 @@ using WorkoutApplication.Data;
 namespace WorkoutApplication.Data.Migrations
 {
     [DbContext(typeof(WorkoutsContext))]
-    partial class WorkoutsContextModelSnapshot : ModelSnapshot
+    [Migration("20171015141712_AddSystemUser")]
+    partial class AddSystemUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WorkoutApplication.Data/Migrations/20171015141712_AddSystemUser.cs
@@ -0,0 +1,389 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WorkoutApplication.Data.Migrations
+{
+    public partial class AddSystemUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "Workouts",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "Workouts",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "Users",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "TargetAreas",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "TargetAreas",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "Sets",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "Sets",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "Resistances",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "Resistances",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "ResistanceBands",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "ResistanceBands",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "ExerciseTargetAreaLinks",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "ExerciseTargetAreaLinks",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "Exercises",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "Exercises",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "ExecutedWorkouts",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "ExecutedWorkouts",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "ExecutedSets",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "ExecutedSets",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "ExecutedExercises",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "ExecutedExercises",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            using (var context = new WorkoutsContext())
+            { 
+                var systemUserId = AddUserForSystemOperations(context);
+                UpdateTargetAreasWithSystemUserId(context, systemUserId);
+                AddForeignKeyToTargetAreas(migrationBuilder);
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "Workouts",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "Workouts",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "TargetAreas",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "TargetAreas",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "Sets",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "Sets",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "Resistances",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "Resistances",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "ResistanceBands",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "ResistanceBands",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "ExerciseTargetAreaLinks",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "ExerciseTargetAreaLinks",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "Exercises",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "Exercises",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "ExecutedWorkouts",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "ExecutedWorkouts",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "ExecutedSets",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "ExecutedSets",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ModifiedDateTime",
+                table: "ExecutedExercises",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ModifiedByUserId",
+                table: "ExecutedExercises",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            //No DOWN operations for adding user for system operations
+        }
+
+        private int AddUserForSystemOperations(WorkoutsContext context)
+        {
+            if (!context.Users.Any(x => x.Name == "SYSTEM"))
+            { 
+                context.Users.Add(
+                    new Domain.User
+                    {
+                        Id = 0,
+                        CreatedByUserId = 0,
+                        CreatedDateTime = DateTime.Now,
+                        Name = "SYSTEM"
+                    });
+
+                context.SaveChanges();
+            }
+
+            return context.Users.First(x => x.Name == "SYSTEM").Id;
+        }
+
+        private void UpdateTargetAreasWithSystemUserId(WorkoutsContext context, int systemUserId)
+        {
+            foreach (var targetArea in context.TargetAreas.Where(x => x.CreatedByUserId == 0))
+            {
+                targetArea.CreatedByUserId = systemUserId;
+            }
+
+            context.SaveChanges();
+        }
+
+        private void AddForeignKeyToTargetAreas(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey("FK_Users", "TargetAreas", "CreatedByUserId", "Users", "dbo", "dbo", "Id");
+        }
+    }
+}
