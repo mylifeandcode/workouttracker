edd7d58ab16e2be3a1c142fa38256f0f26fed0db
Alan Hummel <mylifeandcode@gmail.com>
2019-03-11 12:13:29 -0400

    Updated to ASP.NET Core 2.2

--- WorkoutTracker.UI/Controllers/ExerciseController.cs
+++ WorkoutTracker.UI/Controllers/ExerciseController.cs
@@ -30,13 +30,13 @@ namespace WorkoutTracker.UI.Controllers
 
         // GET: api/Exercises
         [HttpGet]
-        public ActionResult<PaginatedResults<Exercise>> Get(int firstRecord, short pageSize, string nameContains = null, string hasTargetAreas = null)
+        public PaginatedResults<Exercise> Get(int firstRecord, short pageSize, string nameContains = null, string hasTargetAreas = null)
         {
             var filter = BuildExerciseFilter(nameContains, hasTargetAreas);
             var result = new PaginatedResults<Exercise>();
             result.TotalCount = _svc.GetTotalCount();
             result.Results = _svc.Get(firstRecord, pageSize, filter);
-            return Ok(result);
+            return result;
         }
 
         // GET api/Exercises/5
