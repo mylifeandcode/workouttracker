3b8a1cdf67f56737058534697487486561a082bd
Alan Hummel <mylifeandcode@gmail.com>
2020-01-07 15:45:30 -0500

    Quick unit test addition

--- WorkoutTracker.UI/workout-tracker/src/app/workouts/workout.service.spec.ts
+++ WorkoutTracker.UI/workout-tracker/src/app/workouts/workout.service.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { WorkoutService } from './workout.service';
+import { Workout } from 'app/models/workout';
 
+const TEST_WORKOUT_ID = "5";
 
 describe('WorkoutService', () => {
   beforeEach(() => 
@@ -15,5 +17,26 @@ describe('WorkoutService', () => {
   it('should be created', () => {
     const service: WorkoutService = TestBed.get(WorkoutService);
     expect(service).toBeTruthy();
+  });
+
+  it('should get workout by ID', () => {
+    const service: WorkoutService = TestBed.get(WorkoutService);
+    const httpMock: HttpTestingController = TestBed.get(HttpTestingController);
+    
+    const expectedResults = new Workout();
+    const workoutId: number = parseInt(TEST_WORKOUT_ID);
+    expectedResults.id = workoutId;
+
+    service.getById(workoutId).subscribe(
+      (workout: Workout) => expect(workout).toEqual(expectedResults), 
+      fail
+    );
+
+    const req = httpMock.expectOne(`http://localhost:5600/api/workouts/${TEST_WORKOUT_ID}`);
+    expect(req.request.method).toEqual('GET');
+
+    // Respond with the mock results
+    req.flush(expectedResults);
+
   });
 });
