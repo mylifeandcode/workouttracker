6721fb7af08bbfa52c934a25f0fc594acce4af99
Alan Hummel <mylifeandcode@gmail.com>
2021-01-09 15:29:36 -0500

    HomeComponent unit test addition

--- WorkoutTracker.UI/workout-tracker/src/app/home/home.component.spec.ts
+++ WorkoutTracker.UI/workout-tracker/src/app/home/home.component.spec.ts
@@ -1,12 +1,16 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
 import { UserService } from 'app/core/user.service';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 class UserServiceMock {
   logOff = jasmine.createSpy('logOff');
 }
+
+@Component({})
+class FakeComponent {}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -15,7 +19,9 @@ describe('HomeComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes(
+          [{path: 'login', component: FakeComponent}]
+        )
       ], 
       declarations: [ HomeComponent ], 
       providers: [
@@ -37,4 +43,22 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should allow user to log off', () => {
+    
+    //ARRANGE
+    const userService = TestBed.inject(UserService);
+    const router = TestBed.inject(Router);
+
+    spyOn(router, 'navigate').and.callThrough();
+    
+    //ACT
+    component.logOff();
+
+    //ASSERT
+    expect(userService.logOff).toHaveBeenCalledTimes(1);
+    expect(router.navigate).toHaveBeenCalledWith(['login']);
+    
+  });
+
 });
