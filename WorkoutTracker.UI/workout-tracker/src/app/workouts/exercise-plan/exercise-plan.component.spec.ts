bd0f6272e5682019e6911df15f22e0ca20aac6f5
Alan Hummel <ahummel@abim.org>
2022-11-16 15:58:57 -0500

    Implemented 'Use Suggestions' button in ExercisePlanComponent

--- WorkoutTracker.UI/workout-tracker/src/app/workouts/exercise-plan/exercise-plan.component.spec.ts
+++ WorkoutTracker.UI/workout-tracker/src/app/workouts/exercise-plan/exercise-plan.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { UntypedFormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UntypedFormBuilder, FormGroup, ReactiveFormsModule, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { IExercisePlanFormGroup } from '../interfaces/i-exercise-plan-form-group';
 
 import { ExercisePlanComponent } from './exercise-plan.component';
 
@@ -18,6 +19,32 @@ describe('ExercisePlanComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ExercisePlanComponent);
     component = fixture.componentInstance;
+    const formBuilder = new FormBuilder();
+    component.formGroup = formBuilder.group<IExercisePlanFormGroup>({
+      exerciseInWorkoutId: new FormControl<number>(0, { nonNullable: true, validators: Validators.required }), 
+      exerciseId: new FormControl<number>(0, { nonNullable: true, validators: Validators.required }), 
+      exerciseName: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+      numberOfSets: new FormControl<number>(0, { nonNullable: true, validators: Validators.required }),
+      setType: new FormControl<number>(1, { nonNullable: true, validators: Validators.required }),
+      resistanceType: new FormControl<number>(1, { nonNullable: true, validators: Validators.required }),
+      sequence: new FormControl<number>(0, { nonNullable: true, validators: Validators.required }),
+      targetRepCountLastTime: new FormControl<number | null>(0), 
+      avgActualRepCountLastTime: new FormControl<number | null>(0),
+      avgRangeOfMotionLastTime: new FormControl<number | null>(0), 
+      avgFormLastTime: new FormControl<number | null>(0), 
+      recommendedTargetRepCount: new FormControl<number | null>(0), 
+      targetRepCount: new FormControl<number | null>(0, { validators: Validators.min(1) }),
+      resistanceAmountLastTime: new FormControl<number | null>(0), 
+      resistanceMakeupLastTime: new FormControl<string | null>(null), 
+      recommendedResistanceAmount: new FormControl<number | null>(0),
+      recommendedResistanceMakeup: new FormControl<string | null>(null), 
+      resistanceAmount: new FormControl<number>(0, { nonNullable: true, validators: (Validators.min(0.1))} ), 
+      resistanceMakeup: new FormControl<string | null>(null), 
+      bandsEndToEnd: new FormControl<boolean | null>(null), 
+      involvesReps: new FormControl<boolean>(true, { nonNullable: true }),
+      recommendationReason: new FormControl<string | null>(null)
+    });
+
     fixture.detectChanges();
   });
 
@@ -28,8 +55,7 @@ describe('ExercisePlanComponent', () => {
   it('should apply last resistance values when user chooses to', () => {
 
     //ARRANGE
-    const formBuilder = new UntypedFormBuilder();
-    component.formGroup = formBuilder.group({
+    component.formGroup.patchValue({
       resistanceAmountLastTime: 50, 
       resistanceMakeupLastTime: 'Mauve, Tiel', 
       resistanceAmount: 0, 
@@ -40,8 +66,31 @@ describe('ExercisePlanComponent', () => {
     component.useSameResistanceAsLastTime();
 
     //ASSERT
-    expect(component.formGroup.controls['resistanceAmount'].value).toBe(50);
-    expect(component.formGroup.controls['resistanceMakeup'].value).toBe('Mauve, Tiel');
+    expect(component.formGroup.controls.resistanceAmount.value).toBe(50);
+    expect(component.formGroup.controls.resistanceMakeup.value).toBe('Mauve, Tiel');
 
   });
+
+  it('should apply suggestion values when user chooses to', () => {
+
+    //ARRANGE
+    component.formGroup.patchValue({
+      resistanceAmountLastTime: 50, 
+      resistanceMakeupLastTime: 'Mauve, Tiel', 
+      resistanceAmount: 0, 
+      resistanceMakeup: '', 
+      recommendedResistanceAmount: 60, 
+      recommendedResistanceMakeup: 'Aqua', 
+      recommendedTargetRepCount: 8
+    });
+
+    //ACT
+    component.useSuggestions();
+
+    //ASSERT
+    expect(component.formGroup.controls.resistanceAmount.value).toBe(60);
+    expect(component.formGroup.controls.resistanceMakeup.value).toBe('Aqua');
+    expect(component.formGroup.controls.recommendedTargetRepCount.value).toBe(8);
+
+  });  
 });
