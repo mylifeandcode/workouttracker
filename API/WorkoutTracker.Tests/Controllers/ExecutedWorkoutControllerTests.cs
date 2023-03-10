525952abc32e712a931ce40d97aa79a86e3039eb
Alan Hummel <ahummel@abim.org>
2023-03-10 16:00:00 -0500

    Unit tests in progress

--- API/WorkoutTracker.Tests/Controllers/ExecutedWorkoutControllerTests.cs
+++ API/WorkoutTracker.Tests/Controllers/ExecutedWorkoutControllerTests.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using WorkoutTracker.API.Controllers;
 using WorkoutTracker.API.Mappers;
+using WorkoutTracker.API.Models;
 using WorkoutTracker.Application.Workouts.Interfaces;
 using WorkoutTracker.Domain.Workouts;
 
@@ -16,7 +19,8 @@ namespace WorkoutTracker.Tests.Controllers
         private Mock<IExecutedWorkoutDTOMapper> _executedWorkoutDTOMapper;
         private Mock<IExecutedWorkoutSummaryDTOMapper> _executedWorkoutSummaryDTOMapper;
 
-        private IEnumerable<ExecutedWorkout> _inProgressWorkouts;
+        private List<ExecutedWorkout> _inProgressWorkouts;
+        private IEnumerable<ExecutedWorkoutSummaryDTO> _executedWorkoutSummaries;
 
         private ExecutedWorkoutController _sut;
 
@@ -39,17 +43,23 @@ namespace WorkoutTracker.Tests.Controllers
             //ARRANGE
 
             //ACT
-            var results = _sut.GetInProgress();
+            var result = _sut.GetInProgress();
 
             //ASSERT
-            CollectionAssert.AreEquivalent(_inProgressWorkouts, results);
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
+            Assert.AreEqual(_inProgressWorkouts.Count, (result as ActionResult<WorkoutTracker.API.Models.ExecutedWorkoutSummaryDTO[]>).Value.Length);
             _executedWorkoutService.Verify(x => x.GetInProgress(Convert.ToInt32(USER_ID)), Times.Once);
         }
 
         private void SetupExecutedWorkoutServiceMock()
         {
             _executedWorkoutService = new Mock<IExecutedWorkoutService>(MockBehavior.Strict);
-            _inProgressWorkouts = new List<ExecutedWorkout>(0);
+            _inProgressWorkouts = new List<ExecutedWorkout>(5);
+            for (short x = 0; x < 5; x++)
+            {
+                _inProgressWorkouts.Add(new ExecutedWorkout());
+            }
             _executedWorkoutService.Setup(x => x.GetInProgress(It.IsAny<int>())).Returns(_inProgressWorkouts);
         }
 
@@ -61,6 +71,14 @@ namespace WorkoutTracker.Tests.Controllers
         private void SetupExecutedWorkoutSummaryDTOMapper()
         {
             _executedWorkoutSummaryDTOMapper = new Mock<IExecutedWorkoutSummaryDTOMapper>(MockBehavior.Strict);
+            _executedWorkoutSummaries = new List<ExecutedWorkoutSummaryDTO>(0);
+            _executedWorkoutSummaryDTOMapper
+                .Setup(x => x.MapFromExecutedWorkout(It.IsAny<ExecutedWorkout>()))
+                .Returns(new ExecutedWorkoutSummaryDTO(
+                    0, "Sample Workout", 1, 
+                    new DateTime(2023, 5, 6, 12, 0, 0), 
+                    new DateTime(2023, 5, 6, 13, 0, 0), 
+                    new DateTime(2023, 5, 6, 11, 58, 0), "Some notes"));
         }
     }
 }
