f41365103f46f2c26f9c482492e457795206a91e
Alan Hummel <ahummel@abim.org>
2023-10-13 15:49:34 -0400

    Unit test fixes

--- workout-tracker/src/app/workouts/exercise-plan-next-time/exercise-plan-next-time.component.spec.ts
+++ workout-tracker/src/app/workouts/exercise-plan-next-time/exercise-plan-next-time.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExercisePlanNextTimeComponent } from './exercise-plan-next-time.component';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { IExercisePlanFormGroup } from '../interfaces/i-exercise-plan-form-group';
 
 describe('ExercisePlanNextTimeComponent', () => {
   let component: ExercisePlanNextTimeComponent;
@@ -12,8 +14,35 @@ describe('ExercisePlanNextTimeComponent', () => {
     });
     fixture = TestBed.createComponent(ExercisePlanNextTimeComponent);
     component = fixture.componentInstance;
+
+    const formBuilder = new FormBuilder();
+    component.formGroup = formBuilder.group<IExercisePlanFormGroup>({
+      exerciseInWorkoutId: new FormControl<number>(0, { nonNullable: true, validators: Validators.required }), 
+      exerciseId: new FormControl<number>(0, { nonNullable: true, validators: Validators.required }), 
+      exerciseName: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+      numberOfSets: new FormControl<number>(0, { nonNullable: true, validators: Validators.required }),
+      setType: new FormControl<number>(1, { nonNullable: true, validators: Validators.required }),
+      resistanceType: new FormControl<number>(1, { nonNullable: true, validators: Validators.required }),
+      sequence: new FormControl<number>(0, { nonNullable: true, validators: Validators.required }),
+      targetRepCountLastTime: new FormControl<number | null>(0), 
+      avgActualRepCountLastTime: new FormControl<number | null>(0),
+      avgRangeOfMotionLastTime: new FormControl<number | null>(0), 
+      avgFormLastTime: new FormControl<number | null>(0), 
+      recommendedTargetRepCount: new FormControl<number | null>(0), 
+      targetRepCount: new FormControl<number | null>(0, { validators: Validators.min(1) }),
+      resistanceAmountLastTime: new FormControl<number | null>(0), 
+      resistanceMakeupLastTime: new FormControl<string | null>(null), 
+      recommendedResistanceAmount: new FormControl<number | null>(0),
+      recommendedResistanceMakeup: new FormControl<string | null>(null), 
+      resistanceAmount: new FormControl<number>(0, { nonNullable: true, validators: (Validators.min(0.1))} ), 
+      resistanceMakeup: new FormControl<string | null>(null), 
+      bandsEndToEnd: new FormControl<boolean | null>(null), 
+      involvesReps: new FormControl<boolean>(true, { nonNullable: true }),
+      recommendationReason: new FormControl<string | null>(null)
+    });
+
     fixture.detectChanges();
-    //TODO: Add code to set FormGroup
+
   });
 
   it('should create', () => {
--- workout-tracker/src/app/workouts/exercise-plan-suggestions/exercise-plan-suggestions.component.spec.ts
+++ workout-tracker/src/app/workouts/exercise-plan-suggestions/exercise-plan-suggestions.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExercisePlanSuggestionsComponent } from './exercise-plan-suggestions.component';
+import { IExercisePlanFormGroup } from '../interfaces/i-exercise-plan-form-group';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 describe('ExercisePlanSuggestionsComponent', () => {
   let component: ExercisePlanSuggestionsComponent;
@@ -12,8 +14,35 @@ describe('ExercisePlanSuggestionsComponent', () => {
     });
     fixture = TestBed.createComponent(ExercisePlanSuggestionsComponent);
     component = fixture.componentInstance;
+
+    const formBuilder = new FormBuilder();
+    component.formGroup = formBuilder.group<IExercisePlanFormGroup>({
+      exerciseInWorkoutId: new FormControl<number>(0, { nonNullable: true, validators: Validators.required }), 
+      exerciseId: new FormControl<number>(0, { nonNullable: true, validators: Validators.required }), 
+      exerciseName: new FormControl<string>('', { nonNullable: true, validators: Validators.required }),
+      numberOfSets: new FormControl<number>(0, { nonNullable: true, validators: Validators.required }),
+      setType: new FormControl<number>(1, { nonNullable: true, validators: Validators.required }),
+      resistanceType: new FormControl<number>(1, { nonNullable: true, validators: Validators.required }),
+      sequence: new FormControl<number>(0, { nonNullable: true, validators: Validators.required }),
+      targetRepCountLastTime: new FormControl<number | null>(0), 
+      avgActualRepCountLastTime: new FormControl<number | null>(0),
+      avgRangeOfMotionLastTime: new FormControl<number | null>(0), 
+      avgFormLastTime: new FormControl<number | null>(0), 
+      recommendedTargetRepCount: new FormControl<number | null>(0), 
+      targetRepCount: new FormControl<number | null>(0, { validators: Validators.min(1) }),
+      resistanceAmountLastTime: new FormControl<number | null>(0), 
+      resistanceMakeupLastTime: new FormControl<string | null>(null), 
+      recommendedResistanceAmount: new FormControl<number | null>(0),
+      recommendedResistanceMakeup: new FormControl<string | null>(null), 
+      resistanceAmount: new FormControl<number>(0, { nonNullable: true, validators: (Validators.min(0.1))} ), 
+      resistanceMakeup: new FormControl<string | null>(null), 
+      bandsEndToEnd: new FormControl<boolean | null>(null), 
+      involvesReps: new FormControl<boolean>(true, { nonNullable: true }),
+      recommendationReason: new FormControl<string | null>(null)
+    });
+
     fixture.detectChanges();
-    //TODO: Set formGroup
+
   });
 
   it('should create', () => {
@@ -41,6 +70,6 @@ describe('ExercisePlanSuggestionsComponent', () => {
     expect(component.formGroup.controls.resistanceMakeup.value).toBe('Aqua');
     expect(component.formGroup.controls.recommendedTargetRepCount.value).toBe(8);
 
-  });  
-
+  });
+  
 });
