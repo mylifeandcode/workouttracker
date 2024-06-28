afdf985aefdc3c7011c31f1ee89a5ae05cce88bf
Alan Hummel <ahummel@abim.org>
2024-06-28 15:17:51 -0400

    Continued work moving to new control flow

--- workout-tracker/src/app/app.module.ts
+++ workout-tracker/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/
 import { ReactiveFormsModule } from '@angular/forms';
 
 //Third-Party imports
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 //My imports
 //Modules
@@ -15,52 +17,51 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
 //Services
+import { AuthService } from './core/services/auth/auth.service';
 import { ConfigService } from './core/services/config/config.service';
 import { UserService } from './core/services/user/user.service';
 
 //Components
 import { AppComponent } from './app.component';
 
-//Other
-import { AuthService } from './core/services/auth/auth.service';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-
-
 function initializeApp(
-  configService: ConfigService, 
-  userService: UserService, 
-  authService: AuthService, 
+  configService: ConfigService,
+  userService: UserService,
+  authService: AuthService,
   http: HttpClient): () => Observable<any> {
-    console.log("APP IS INITIALIZING...");
-    return (): Observable<object> => http.get("config.json")
-      .pipe(
-        tap((config: object) => {
-          console.log("Loaded config: ", config);
-          configService.init(config);
-          authService.init();
-          authService.restoreUserSessionIfApplicable(); 
-          userService.init();
-        })
-      );
+  console.log("APP IS INITIALIZING...");
+  return (): Observable<object> => http.get("config.json")
+    .pipe(
+      tap((config: object) => {
+        console.log("Loaded config: ", config);
+        configService.init(config);
+        authService.init();
+        authService.restoreUserSessionIfApplicable();
+        userService.init();
+      })
+    );
 }
 
-@NgModule({ declarations: [
-        AppComponent
-    ],
-    bootstrap: [AppComponent], imports: [AppRoutingModule,
-        BrowserAnimationsModule,
-        BrowserModule,
-        CommonModule,
-        CoreModule,
-        ReactiveFormsModule,
-        SharedModule], providers: [
-        {
-            provide: APP_INITIALIZER,
-            useFactory: initializeApp,
-            deps: [ConfigService, UserService, AuthService, HttpClient],
-            multi: true
-        },
-        provideHttpClient(withInterceptorsFromDi())
-    ] })
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [AppComponent], 
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    CommonModule,
+    CoreModule,
+    ReactiveFormsModule,
+    SharedModule], providers: [
+      {
+        provide: APP_INITIALIZER,
+        useFactory: initializeApp,
+        deps: [ConfigService, UserService, AuthService, HttpClient],
+        multi: true
+      },
+      provideHttpClient(withInterceptorsFromDi())
+    ]
+})
 export class AppModule { }
