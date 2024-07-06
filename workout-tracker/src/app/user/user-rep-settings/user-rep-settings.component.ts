25f934687920fe55784239edb1565a76ae98c2d7
Alan Hummel <slade73@hotmail.com>
2024-07-06 14:22:32 -0400

    Continued refactoring for new control flow

--- workout-tracker/src/app/user/user-rep-settings/user-rep-settings.component.ts
+++ workout-tracker/src/app/user/user-rep-settings/user-rep-settings.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { SetType } from 'app/workouts/enums/set-type';
 
 export interface IRepSettingsForm {
   repSettingsId: FormControl<number>;
@@ -19,6 +20,7 @@ export class UserRepSettingsComponent implements OnInit {
   @Input()
   repSettingsFormGroup!: FormGroup<IRepSettingsForm>; //WARN: use of ! -- look for a better solution
 
+  public setTypeEnum: typeof SetType = SetType;
 
   constructor() { }
 
