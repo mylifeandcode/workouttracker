295dd4f4cbd555f70d8e4f0741a72fe712eaf94e
Alan Hummel <slade73@hotmail.com>
2025-05-24 15:45:32 -0400

    More post-PrimeNG removal clean-up. Fixed table load for workouts list. Added debounce capability for workouts list signals.

--- workout-tracker/src/app/workouts/workout-list/workout-list.component.ts
+++ workout-tracker/src/app/workouts/workout-list/workout-list.component.ts
@@ -3,12 +3,14 @@ import { WorkoutService } from '../_services/workout.service';
 import { WorkoutDTO } from 'app/workouts/_models/workout-dto';
 import { PaginatedResults } from '../../core/_models/paginated-results';
 import { debounceTime, finalize } from 'rxjs/operators';
+import { combineLatest } from 'rxjs';
 import { HttpResponse } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
 import { NzTableModule, NzTableQueryParams } from 'ng-zorro-antd/table';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { FormsModule } from '@angular/forms';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'wt-workout-list',
@@ -16,57 +18,37 @@ import { FormsModule } from '@angular/forms';
     styleUrls: ['./workout-list.component.scss'],
     imports: [FormsModule, NzTableModule, NzIconModule, NzDropDownModule, RouterLink]
 })
-export class WorkoutListComponent implements OnInit {
-  private _workoutSvc = inject(WorkoutService);
-
-
-  //There is no ngOnInit or ngAfterViewInit here because the onLazyLoad() event of the PrimeNg
-  //Turbo Table automatically makes a call to get data on initialization
+export class WorkoutListComponent {
+  private readonly _workoutSvc = inject(WorkoutService);
 
   public totalRecords: number = 0;
   public loading: boolean = true;
-  public pageSize: number = 10;
   public pageIndex: number = 1;
   public workouts: WorkoutDTO[] = [];
 
-
-  public workoutNameFilterVisible: boolean = false;
-
   protected _nameFilter = signal('');
   protected _filterByActiveOnly = signal(true);
-  /*
-  protected _filterChange = effect(() => {
-    //Even though the signals aren't referenced here, getWorkouts() references them, 
-    //so this effect will run when they change :)
-    //debounceTime(300);
-    console.log('EFFECT!');
-    this.getWorkouts(0); 
+  protected _pageSize: number = 10;
+
+  //Signals don't have debounce natively, so we need this ugly bit of code :(
+  private _nameFilterForDebounce$ = toObservable(this._nameFilter).pipe(debounceTime(500));
+  private _nameFilterForDebounce = toSignal(this._nameFilterForDebounce$);
+
+  private _filterChange = effect(() => {
+    //Even if the signals weren't referenced here, getWorkouts() references them, 
+    //so this effect would run when they change :)
+    this.getWorkouts(0, this._pageSize, this._filterByActiveOnly(), this._nameFilterForDebounce());
   });
-  */
 
-  public filterStatus = [
-    { text: 'Active Only', value: [true], byDefault: true }
-  ];
-
-  public ngOnInit(): void {
-    //this.loading = true;
-    //this.getWorkouts(0);
-  }
-
-  public getWorkouts(first: number, pageSize: number = 10): void {
-    //console.log("GETTING WORKOUTS: ", first, pageSize);
-    this.totalRecords = 0;
-    this.loading = true;
-    this._workoutSvc.getFilteredSubset(first, pageSize, this._filterByActiveOnly(), this._nameFilter())
-      .pipe(finalize(() => { this.loading = false; }))
-      .subscribe({
-        next: (results: PaginatedResults<WorkoutDTO>) => {
-          this.workouts = results.results;
-          this.totalRecords = results.totalCount;
-        },
-        error: (error: any) => window.alert("An error occurred getting workouts: " + error)
-      });
-  }
+  public onQueryParamsChange(params: NzTableQueryParams): void {
+    console.log("Table parameters have changed: ", params);
+    const { pageSize, pageIndex, sort, filter } = params;
+    this._pageSize = pageSize;
+    //const currentSort = sort.find(item => item.value !== null);
+    //const sortField = (currentSort && currentSort.key) || null;
+    //const sortOrder = (currentSort && currentSort.value) || null;
+    this.getWorkouts(((pageIndex - 1) * pageSize), pageSize);
+  }  
 
   public retireWorkout(workoutPublicId: string, workoutName: string): void {
     if (window.confirm(`Are you sure you want to retire workout "${workoutName}"?`)) {
@@ -96,49 +78,18 @@ export class WorkoutListComponent implements OnInit {
     }
   }
 
-  /*
-  //TODO: Find out if I can consolidate these 2 methods into a generic one and call it from HTML (those brackets may cause problems)
-  //TODO: Consolidate these into a service. These are copied from another component. :(
-  public filterTableByInput(table: Table, event: Event, filterOn: string, filterType: string = 'in'): void {
-    //console.log("EVENT: ", event);
-    table.filter((event.target as HTMLInputElement).value, filterOn, filterType);
+  private getWorkouts(first: number, pageSize: number = 10, filterByActiveOnly: boolean = false, nameFilter: string = ''): void {
+    console.log("GETTING WORKOUTS: ", first, pageSize);
+    //this.totalRecords = 0; DO NOT SET THIS -- IT WILL TRIGGER THE PARAMS CHANGE HANDLER AND CALL IT ALL AGAIN WITH THE DEFAULT PARAMS!
+    this.loading = true;
+    this._workoutSvc.getFilteredSubset(first, pageSize, filterByActiveOnly, nameFilter)
+      .pipe(finalize(() => { this.loading = false; }))
+      .subscribe({
+        next: (results: PaginatedResults<WorkoutDTO>) => {
+          this.workouts = results.results;
+          this.totalRecords = results.totalCount;
+        },
+        error: (error: any) => window.alert("An error occurred getting workouts: " + error)
+      });
   }
-
-  public filterTableByActive(table: Table, event: Event): void {
-    //console.log("CHECKBOX EVENT: ", event);
-    table.filter((event.target as HTMLInputElement).checked, 'activeOnly', 'equals');
-  }
-  */
-
-  public onQueryParamsChange(params: NzTableQueryParams): void {
-    console.log("Table parameters have changed: ", params);
-    const { pageSize, pageIndex, sort, filter } = params;
-    const currentSort = sort.find(item => item.value !== null);
-    const sortField = (currentSort && currentSort.key) || null;
-    const sortOrder = (currentSort && currentSort.value) || null;
-    //this._filterByActiveOnly = filter.indexOf((filterItem) => filterItem.key == 'active') > -1 ? true : false;
-    //this.loadDataFromServer(pageIndex, pageSize, sortField, sortOrder, filter);
-    this.getWorkouts((pageIndex - 1) * pageSize, pageSize);
-  }  
-
-  /*
-  public reset(): void {
-    this._nameFilter.set('');
-    this.search();
-  }
-
-  public search(): void {
-    this.workoutNameFilterVisible = false;
-    this.getWorkouts(0);
-  }
-  */
- 
-  public filterTableByWorkoutName(): void {
-
-  }
-
-  public filterTableByWorkoutStatus(): void {
-
-  }
-
 }
